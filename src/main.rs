4debfa7910ccd96ee5578b16c87b0eb1336b3a95
Alessio <53220763+Atari2@users.noreply.github.com>
2023-11-04 22:58:07 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,61 @@
 mod onnxparser;
-use std::io::Read;
+
+use std::{path::Path, io::Read};
 
 use onnxparser::onnx;
+use walkdir::WalkDir;
+
+fn read_model_binary(p: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let file = std::fs::File::open(p)?;
+    let mut reader = std::io::BufReader::new(file);
+    let _model: onnx::ModelProto = protobuf::Message::parse_from_reader(&mut reader)?;
+    // for graph in model.graph.iter() {
+    //     for (i, node) in graph.node.iter().enumerate() {
+    //         match node.op_type {
+    //             Some(ref op_type) => match op_type.as_str() {
+    //                 "MatMul" => {
+    //                     println!("{}: MatMul", i);
+    //                 }
+    //                 "Add" => {
+    //                     println!("{}: Add", i);
+    //                 }
+    //                 "Conv" => {
+    //                     println!("{}: Conv", i);
+    //                 }
+    //                 _ => println!("Unimplemented op: {}", op_type),
+    //             },
+    //             None => println!("{}: <unknown>", i),
+    //         }
+    //     }
+    // }
+    Ok(())
+}
+
+fn read_model_text(p: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let file = std::fs::File::open(p)?;
+    let mut reader = std::io::BufReader::new(file);
+    let mut buf = String::new();
+    reader.read_to_string(&mut buf)?;
+    let _model: onnx::ModelProto = protobuf::text_format::parse_from_str(&buf)?;
+    Ok(())
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = std::fs::File::open("examples/test.onnx")?;
-    let mut str = String::new();
-    file.read_to_string(&mut str)?;
-    let model = protobuf::text_format::parse_from_str::<onnx::ModelProto>(&str)?;
-    for graph in model.graph.iter() {
-        for (i, node) in graph.node.iter().enumerate() {
-            println!("node: {}, op_type: {}", i, node.op_type.as_ref().unwrap());
-            node.input.iter().for_each(|input| println!("\tinput: {}", input));
-            node.output.iter().for_each(|output| println!("\toutput: {}", output));
+    for entry in WalkDir::new("examples") {
+        let entry = entry?;
+        if entry.path().extension().map_or(false, |e| e == "onnx") {
+            match read_model_binary(entry.path()) {
+                Ok(_) => println!("{}: OK", entry.path().display()),
+                Err(e) => {
+                    println!("Error during binary parsing of {}: {}, trying with text", entry.path().display(), e);
+                    match read_model_text(entry.path()) {
+                        Ok(_) => println!("{}: OK", entry.path().display()),
+                        Err(e) => println!("Error during text parsing of {}: {}", entry.path().display(), e),
+                    }
+                },
+            }
         }
     }
+
     Ok(())
 }
