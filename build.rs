33c5122e92c4d303489c37744ac4563df71286b9
Atari2 <alex.robassomero@gmail.com>
2023-12-20 11:26:20 +0100

    Remove heavy models
    Download them at build time

--- build.rs
+++ build.rs
@@ -1,9 +1,44 @@
-use std::env;
+use curl::easy::Easy;
+use std::{env, io::Write, path::PathBuf};
+use zip_extensions::zip_extract;
 
 use protobuf_codegen::Customize;
 
 fn main() {
     // Use this in build.rs
+
+    // models contained in models.zip
+    let modelnames = [
+        "vgg19-7",
+        "GPT2",
+        "zfnet512-12",
+        "caffenet-12",
+        "bvlcalexnet-12",
+    ];
+
+    if modelnames
+        .iter()
+        .all(|modelname| std::path::Path::new("models").join(modelname).exists())
+    {
+        println!("models already downloaded");
+    } else {
+        println!("downloading models");
+        let mut easy = Easy::new();
+        easy.url("https://www.atarismwc.com/models.zip").unwrap();
+        let file = std::fs::File::create("models/models.zip").unwrap();
+        let mut writer = std::io::BufWriter::new(file);
+        easy.write_function(move |data| {
+            writer.write_all(data).unwrap();
+            Ok(data.len())
+        })
+        .unwrap();
+        easy.perform().unwrap();
+        let archive_file: PathBuf = "models/models.zip".into();
+        let target_dir: PathBuf = "models".into();
+        zip_extract(&archive_file, &target_dir).unwrap();
+        std::fs::remove_file("models/models.zip").unwrap();
+    }
+
     env::set_var("OUT_DIR", "src");
     println!("cargo:rerun-if-changed=src/protos/onnx.proto");
     protobuf_codegen::Codegen::new()
